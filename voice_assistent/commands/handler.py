4410fc298011df4b5694c48fa5b54534e7175817
Igor Faustino <igornfaustino@gmail.com>
2019-11-05 12:53:34 +0000

    handle intents based on predict

--- voice_assistent/commands/handler.py
+++ voice_assistent/commands/handler.py
@@ -1,26 +1,20 @@
 from . import date, applications
 from common.voice import speak
+from learning import intents
 
-CHECK_CALENDAR_TRIGGERS = [
-    'do i have plans',
-    'what do i have',
-    'am i busy',
-]
-
-SEARCH_TRIGGERS = [
-    'search for',
-]
+intent_predictor = intents.Intents()
+intent_predictor.train()
 
 
 def handler(text):
     if not text:
         return
 
-    for trigger in CHECK_CALENDAR_TRIGGERS:
-        if trigger in text:
-            return date.handle_check_plans(text)
-
-    for trigger in SEARCH_TRIGGERS:
-        if trigger in text:
-            return applications.handle_search(trigger, text)
-    speak(text)
+    intent = intent_predictor.predict(text)
+    print(intent)
+    if not intent:
+        return speak(text)
+    if intent == "check_calendar":
+        return date.handle_check_plans(text)
+    if intent == "search":
+        return speak("search")
